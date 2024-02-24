449b8e1be8e7aa5cc6d464961b51569972fa8a61
JoMiras <jonah@bayvalleytech.com>
2024-02-24 11:22:29 -0800

    Initial Commit

---
+++ PetFriendX/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PetFriendX
+//
+//  Created by Jonah Mirasol on 2024/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PetFriendX/PetFriendXApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetFriendXApp.swift
+//  PetFriendX
+//
+//  Created by Jonah Mirasol on 2024/2/24.
+//
+
+import SwiftUI
+
+@main
+struct PetFriendXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
