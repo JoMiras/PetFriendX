dedf8e68d22ec53e0cd6eb755c31cea523d01850
JoMiras <jonah@bayvalleytech.com>
2024-02-24 23:47:10 -0800

    For the recommended pets portion, the array is displayed just fine however the buttons do not work at that specific portion.

---
+++ PetFriendX/RecommendedPets.swift
@@ -0,0 +1,18 @@
+//
+//  RecommendedPets.swift
+//  PetFriendX
+//
+//  Created by Jonah Mirasol on 2024/2/24.
+//
+
+import SwiftUI
+
+struct RecommendedPets: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecommendedPets()
+}
